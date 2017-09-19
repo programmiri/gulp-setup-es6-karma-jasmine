b1478d3ecf021ead0feebd9085c607112ed016e7
programmiri <programmiri@gmail.com>
2017-09-19 17:48:40 +0200

    removed unnecessary comments

--- karma.conf.js
+++ karma.conf.js
@@ -4,78 +4,45 @@
 module.exports = function(config) {
   config.set({
 
-  // base path that will be used to resolve all patterns (eg. files, exclude)
-  basePath: '',
+    basePath: '',
 
-  // frameworks to use
-  // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-  frameworks: ['browserify', 'jasmine-jquery','jasmine'],
+    frameworks: ['browserify', 'jasmine-jquery','jasmine'],
 
+    files: [
+      'assets/spec/**/*test.js'
+      ],
 
-  files: [
-    'assets/spec/**/*test.js'
-    ],
+    preprocessors: {
+      'assets/spec/**/*test.js' : ['browserify']
+    },
 
+    browserify: {
+      debug: true,
+      transform: [
+        ['babelify',{
+          presets: ['es2015']
+        }]
+      ]
+    },
 
-  // list of files to exclude
-  exclude: [
-  ],
-
-
-  // preprocess matching files before serving them to the browser
-  // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-  preprocessors: {
-    'assets/spec/**/*test.js' : ['browserify']
-  },
-
-  browserify: {
-    debug: true,
-    transform: [
-      ['babelify',{
-        presets: ['es2015']
-      }]
-    ]
-  },
+    reporters: ['spec'],
 
 
-  // test results reporter to use
-  // possible values: 'dots', 'progress'
-  // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-  reporters: ['spec'],
+    port: 9876,
 
+    colors: true,
 
-  // web server port
-  port: 9876,
+    logLevel: config.LOG_INFO,
 
+    autoWatch: true,
 
-  // enable / disable colors in the output (reporters and logs)
-  colors: true,
+    browsers: ['Chrome'],
 
+    singleRun: false,
 
-  // level of logging
-  // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-  logLevel: config.LOG_INFO,
+    concurrency: Infinity,
 
+    browserDisconnectTolerance: 2
 
-  // enable / disable watching file and executing tests whenever any file changes
-  autoWatch: true,
-
-
-  // start these browsers
-  // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-  browsers: ['Chrome'],
-
-
-  // Continuous Integration mode
-  // if true, Karma captures browsers, runs the tests and exits
-  singleRun: false,
-
-  // Concurrency level
-  // how many browser should be started simultaneous
-  concurrency: Infinity,
-
-  // see https://github.com/karma-runner/karma/issues/1514
-  // keep the karma browser in the foreground
-  browserDisconnectTolerance: 2
   })
 }
